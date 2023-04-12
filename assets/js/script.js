86af3997eae07bff764e92c4d0984205bfe54e8b
raffaele-catalano <raf.catalano@gmail.com>
2023-04-12 17:58:33 +0200

    added basic markup

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const {createApp} = Vue;
+
+createApp ( {
+    // in data si inseriscono gli oggetti
+    data () {
+        return{
+
+        }
+    },
+    // in methods si inseriscono le funzioni
+    methods: {
+
+    }
+    // con mount ci si collega al tag nell'HTML
+}).mount('#app');
